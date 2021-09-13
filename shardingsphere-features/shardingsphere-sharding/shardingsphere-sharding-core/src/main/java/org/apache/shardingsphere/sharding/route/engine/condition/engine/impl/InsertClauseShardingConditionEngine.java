e7a98c2b0d0e17feaae8dc7e6b1681273e0e52ae
钟显东 <sig1997@foxmail.com>
2021-09-13 19:32:23 +0800

    Update InsertClauseShardingConditionEngine.java

--- shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/main/java/org/apache/shardingsphere/sharding/route/engine/condition/engine/impl/InsertClauseShardingConditionEngine.java
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/main/java/org/apache/shardingsphere/sharding/route/engine/condition/engine/impl/InsertClauseShardingConditionEngine.java
@@ -63,49 +63,75 @@ public final class InsertClauseShardingConditionEngine implements ShardingCondit
         appendGeneratedKeyConditions(sqlStatementContext, result);
         return result;
     }
-    
+
+    /**
+     * 单纯insert没有select语句的创建分片条件ShardingCondition
+     * @param sqlStatementContext sql上下文
+     * @param parameters 参数
+     * @return ShardingCondition列表
+     */
     private List<ShardingCondition> createShardingConditionsWithInsertValues(final InsertStatementContext sqlStatementContext, final List<Object> parameters) {
         List<ShardingCondition> result = new LinkedList<>();
+        //获取sql表名
         String tableName = sqlStatementContext.getSqlStatement().getTable().getTableName().getIdentifier().getValue();
+        //获取sql插入的全部列名
         Collection<String> columnNames = getColumnNames(sqlStatementContext);
+        //遍历插入行集合，insert语句可以插入多行(),()
         for (InsertValueContext each : sqlStatementContext.getInsertValueContexts()) {
+            //把表名、列名、参数传递下去，创建ShardingCondition，并添加到result中
             result.add(createShardingCondition(tableName, columnNames.iterator(), each, parameters));
         }
         return result;
     }
-    
+
+    /**
+     * 获取insert语句中插入的列名
+     * @param insertStatementContext insert sql 上下文
+     * @return 列名集合
+     */
     private Collection<String> getColumnNames(final InsertStatementContext insertStatementContext) {
+        //判断是否有自动生成主键，有的话，列名集合去掉该列
         Optional<GeneratedKeyContext> generatedKey = insertStatementContext.getGeneratedKeyContext();
         if (generatedKey.isPresent() && generatedKey.get().isGenerated()) {
             Collection<String> result = new LinkedList<>(insertStatementContext.getColumnNames());
             result.remove(generatedKey.get().getColumnName());
             return result;
         }
+        //没有自动生成主键，直接获取sql插入的全部列名
         return insertStatementContext.getColumnNames();
     }
     
     private ShardingCondition createShardingCondition(final String tableName, final Iterator<String> columnNames, final InsertValueContext insertValueContext, final List<Object> parameters) {
         ShardingCondition result = new ShardingCondition();
+        //ShardingSphere自己的时间服务
         DatetimeService datetimeService = RequiredSPIRegistry.getRegisteredService(DatetimeService.class);
-        //配置的表达式
+        //遍历每一项插入的value
         for (ExpressionSegment each : insertValueContext.getValueExpressions()) {
             //对应插入的字段名
             String columnName = columnNames.next();
             //如果该字段被配置用来分片
             if (shardingRule.isShardingColumn(columnName, tableName)) {
                 if (each instanceof SimpleExpressionSegment) {
-                    //该值为普通值，则
+                    //拿到这个字段的参数值，添加到result
                     result.getValues().add(new ListShardingConditionValue<>(columnName, tableName, Collections.singletonList(getShardingValue((SimpleExpressionSegment) each, parameters))));
                 } else if (ExpressionConditionUtils.isNowExpression(each)) {
+                    //如果是now()，添加的是DatetimeService的时间
                     result.getValues().add(new ListShardingConditionValue<>(columnName, tableName, Collections.singletonList(datetimeService.getDatetime())));
                 } else if (ExpressionConditionUtils.isNullExpression(each)) {
+                    //分片键值不能为null
                     throw new ShardingSphereException("Insert clause sharding column can't be null.");
                 }
             }
         }
         return result;
     }
-    
+
+    /**
+     * 获取
+     * @param expressionSegment
+     * @param parameters
+     * @return
+     */
     private Comparable<?> getShardingValue(final SimpleExpressionSegment expressionSegment, final List<Object> parameters) {
         Object result;
         if (expressionSegment instanceof ParameterMarkerExpressionSegment) {
@@ -118,7 +144,9 @@ public final class InsertClauseShardingConditionEngine implements ShardingCondit
     }
     
     private List<ShardingCondition> createShardingConditionsWithInsertSelect(final InsertStatementContext sqlStatementContext, final List<Object> parameters) {
+        //获取insert中的select sql上下文
         SelectStatementContext selectStatementContext = sqlStatementContext.getInsertSelectContext().getSelectStatementContext();
+        //创建WhereClauseShardingConditionEngine，调用createShardingConditions方法，
         return new LinkedList<>(new WhereClauseShardingConditionEngine(shardingRule, schema).createShardingConditions(selectStatementContext, parameters));
     }
     
